221f60b9ce2dbc91ff7dfb3b1ad44ab04062d0db
Angiee Karoline Galvis Tarazona <akgalvis@gmail.com>
2025-05-12 08:09:28 -0500

    Update Angiee_Colas.php
    Simplemente cambié el orden de los elementos

--- Angiee_Colas.php
+++ Angiee_Colas.php
@@ -1,16 +1,20 @@
 <?php
 	$hetalia = array();
 	
-	array_push($hetalia, "Italia");
-	array_push($hetalia, "Alemania");
-	array_push($hetalia, "Japon");
 	array_push($hetalia, "Estados Unidos");
+	array_push($hetalia, "Japon");
+	array_push($hetalia, "Suiza");
 	array_push($hetalia, "Inglaterra");
 	array_push($hetalia, "Francia");
 	array_push($hetalia, "China");
 	array_push($hetalia, "Rusia");
+	array_push($hetalia, "Lithuania");
+	array_push($hetalia, "Estonia");
+	array_push($hetalia, "Polonia");
+	array_push($hetalia, "Alemania");
+	array_push($hetalia, "Italia");
 	
 	$elemento = array_shift(array: $hetalia);
 	
-	echo "El primer elemento que llegó a la cola fue " .$elemento
+	echo "En el primer capítulo del anime, la primera persona en aparecer fue " .$elemento
 ?>
